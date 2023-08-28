6efcd2385bcb1e142aeb23be25a47f6553b93454
Jorge Contreras <jorge.contreras789@gmail.com>
2023-08-28 18:40:23 -0400

    created basic server with one Handle at /

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("wassup")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	corsMux := middlewareCors(mux)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	fmt.Printf("Listening at %s", s.Addr)
+	log.Fatal(s.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
